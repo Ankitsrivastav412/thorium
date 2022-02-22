acbc83b997388465f5ee8bfc829c49a77cb3fddb
Ankitsrivastav412 <ankitsrivastava41097@gmail.com>
2022-02-22 23:03:08 +0530

    Assignment1

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+//router.get('/jackie', function (req,res) {
+  //  res.send('my name is Ankit!')
+//});
 router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+ res.send('My first ever api!')
 });
 
 module.exports = router;
