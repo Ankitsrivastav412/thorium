946e75e8d4ef8845feb18a9a2c156539a1d47ca4
Ankitsrivastav412 <ankitsrivastava41097@gmail.com>
2022-03-09 01:39:30 +0530

    this is my mongo Assignment 6

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
+
 const route = require('./routes/route.js');
-const { default: mongoose } = require('mongoose');
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const mongoose = require('mongoose')
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+mongoose.connect("mongodb+srv://Ankitsrivastav412:G1fTVr3JcQfPfLGB@myfunctionup.f4mg0.mongodb.net/Ankit412?authSource=admin&replicaSet=atlas-rbbsrv-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true", 
+{useNewUrlParser: true}
+)
+    .then(() => console.log('mongodb running and connected'))
+    .catch(err => console.log(err))
 
+app.use('/', route);
 
-app.use('/', route)
-
-
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen(process.env.PORT || 3000, function() {
+	console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
